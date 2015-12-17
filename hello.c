dac21da2457cce86575762b42c0b5a71bcee96eb
olivier GERMAIN <olivier.germain@schneider-electric.com>
2015-12-17 16:57:07 +0100

    Adding mult function
    Change-Id: Ic19c5b6e2665b031f7e8962b23257bead309846c

--- hello.c
+++ hello.c
@@ -3,6 +3,7 @@
 int print_hello_world(void);
 int adder(int,int);
 int sub(int,int);
+int mult(int,int);
 
 main()
 {
@@ -10,11 +11,17 @@ main()
     print_hello_world();
     print("%d+%d=%d\n", a,b,adder(a,b));
     print("%d-%d=%d\n", b,a,sub(b,a));
+    print("%d*%d=%d\n", a,b,mult(a,b));
 }
 
 int adder(int a, int b)
 {
     return (a+b);
+}
+
+int mult(int a, int b)
+{
+  return (a*b);
 }
 
 int sub(int a, int b)
