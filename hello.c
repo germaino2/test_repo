99de2e6301dc2bd858f2f83e2a165e5cc7cd0c3d
olivier GERMAIN <olivier.germain@schneider-electric.com>
2015-12-17 15:50:09 +0100

    Add sub function
    New function sub is added
    
    Change-Id: I972e95675a62cf1e31b93eb8ad117f7fec1d7dbc

--- hello.c
+++ hello.c
@@ -2,18 +2,26 @@
 
 int print_hello_world(void);
 int adder(int,int);
+int sub(int,int);
 
 main()
 {
     int a=4,b=5;
     print_hello_world();
     print("%d+%d=%d\n", a,b,adder(a,b));
+    print("%d-%d=%d\n", b,a,sub(b,a));
 }
 
 int adder(int a, int b)
 {
     return (a+b);
 }
+
+int sub(int a, int b)
+{
+  return (a-b);
+}
+
 int print_hello_world()
 {
     printf("Hello World\n");
